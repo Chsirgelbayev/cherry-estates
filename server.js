2a54d7b171561741b26cee7a2c68a4462a26c0e0
Chingiz <csirgilbaev@gmail.com>
2022-04-12 20:53:40 +0600

    Project initialization

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const exphbs = require("express-handlebars");
+const colors = require("colors");
+const morgan = require("morgan");
+
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config/config.env" });
+const { NODE_ENV, PORT, MONGO_URL } = process.env;
+const connectDB = require("./config/db");
+
+const todosRoutes = require("./routes/list");
+
+const app = express();
+
+const hbs = exphbs.create({
+  defaultLayout: "main",
+  extname: "hbs",
+});
+
+connectDB();
+
+if (NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+app.engine("hbs", hbs.engine);
+app.set("view engine", "hbs");
+app.set("views", "views");
+app.use(express.static("public"));
+
+app.use(todosRoutes);
+
+const server = app.listen(PORT || 3000, () => {
+  console.log(
+    `Server is running on ${NODE_ENV} mode and ${PORT} port`.black.bgYellow
+  );
+});
+
+process.on("unhandledRejection", (e) => {
+  console.log(`Error: ${e.message}`.red.underline.bold);
+  server.close(() => process.exit(1));
+});
